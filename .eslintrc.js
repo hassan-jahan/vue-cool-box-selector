dcf0fd724ffa8bd2152bc321eeee1e5b59b3a31d
Hassan Jahan <hassan.jahan@gmail.com>
2023-04-22 19:29:09 +0330

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-  'extends': [
-    '@antfu',
-    // 'eslint:recommended',
-    // "plugin:vue/vue3-strongly-recommended",
-    // 'plugin:vue/vue3-recommended',
-    // 'plugin:vue/recommended', // Use this if you are using Vue.js 2.x.
-    // "prettier"
-    // Make sure "prettier" is the last element in this list
-  ],
-  ignorePatterns: [
-    'node_modules/',
-    'dist/',
-    'coverage/',
-    'pnpm-lock.yaml',
-    '*.js'
-  ]
+    'extends': [
+        '@antfu',
+        // 'eslint:recommended',
+        // "plugin:vue/vue3-strongly-recommended",
+        // 'plugin:vue/vue3-recommended',
+        // 'plugin:vue/recommended', // Use this if you are using Vue.js 2.x.
+        // "prettier"
+        // Make sure "prettier" is the last element in this list
+    ],
+    ignorePatterns: [
+        'node_modules/',
+        'dist/',
+        'coverage/',
+        'pnpm-lock.yaml',
+        '*.js'
+    ],
+    rules: {
+        // "object-curly-spacing": ['off'],
+    }
 }
